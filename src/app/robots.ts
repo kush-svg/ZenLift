396550bd5912f5e1e369216a143d6b61edd81af9
CulMyca <culmyca2k26@jcboseust.ac.in>
2026-05-30 02:25:15 +0530

    Optimize dynamic robots.ts and sitemap.ts on www.zenlift.in to enforce stable favicon crawling

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -4,9 +4,17 @@ export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: '*',
-      allow: '/',
+      allow: [
+        '/',
+        '/favicon.ico',
+        '/icon-48.png',
+        '/icon-96.png',
+        '/icon-192.png',
+        '/icon-512.png',
+        '/apple-touch-icon.png'
+      ],
       disallow: '/api/*',
     },
-    sitemap: 'https://zenlift.in/sitemap.xml',
+    sitemap: 'https://www.zenlift.in/sitemap.xml',
   };
 }
--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from 'next';
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://zenlift.in',
+      url: 'https://www.zenlift.in',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 1,
